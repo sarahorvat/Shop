ae322a2d045290274de5a2760075afc35be88549
sarahorvat <sara.horvat@student.hnu.de>
2025-01-10 17:22:54 +0100

    JUnit test

---
+++ src/test/java/org/example/ShopTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ShopTest {
+
+    @org.junit.jupiter.api.Test
+    void getKleidungspreis() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFarbenpreis() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getGroessenpreis() {
+    }
+}
