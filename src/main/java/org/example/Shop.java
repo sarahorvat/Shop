62c2331a5a84cb69084fd98ca7c1e91a2a082144
SophiaB <sophia.buehren@icloud.com>
2025-01-10 13:50:17 +0100

    GUI form

---
+++ src/main/java/org/example/Shop.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Shop extends JFrame {
+    private JLabel lbl_auswahl;
+    private JPanel frame;
+    private JComboBox combo_auswahl;
+    private JLabel lbl_farbe;
+    private JRadioButton radio_rot;
+    private JRadioButton radio_grün;
+    private JRadioButton radio_blau;
+    private JRadioButton radio_gelb;
+    private JRadioButton radio_schwarz;
+    private JRadioButton radio_pink;
+    private JLabel lbl_größe;
+    private JRadioButton radio_s;
+    private JRadioButton radio_m;
+    private JRadioButton radio_l;
+    private JButton bt_hinzufügen;
+    private JLabel lbl_warenkorb;
+    private JTextArea textArea;
+    private JButton bt_delete;
+    private JLabel lbl_preis;
+    private JTextField tf_preis;
+    private JButton bt_einkaufen;
+    private JCheckBox ckeck_rabatt;
+
+    public Shop() {
+        setTitle("Simple Shop");
+        setContentPane(frame);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(500, 600);
+        setVisible(true);
+        setResizable(false);
+    }
+
+    public static void main(String[] args) {
+        new Shop();
+
+    }
+}
+
+
+
---
+++ src/main/java/org/example/Ware.java
@@ -0,0 +1,4 @@
+package org.example;
+
+public class Ware {
+}
